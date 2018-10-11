8dd0c08be1b13c758ae579fed77fbd3e84592ee8
Mohammad Khodabandeh <Khodabandeh.Mohammad@gmail.com>
2018-10-12 01:47:54 +0330

    [ADD] add deployment.sh deploy nash scripts [FIX[ fix yml

---
+++ .circleci/deployement.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
+
+TASK_NAME=$1
+SERVICE_NAME=$2
+ARG_TSK=$3
+
+aws ecs list-clusters | grep $ECS_CLUSTER_NAME || aws ecs create-cluster --cluster-name $ECS_CLUSTER_NAME
+
+echo "*** ecs {$TASM_NAME} task initiating ... ***"  
+
+export TASK_VERSION=$(aws ecs register-task-definition --family $TASK_NAME --requires-compatibilities FARGATE --cpu 256 --memory 1024 --network-mode awsvpc --container-definitions '[{"name":"$DKR_IMG","image":"$DKR_IMG:latest","entryPoint":["dotnet", "Avt.Agents.Services.dll","--ServiceName {$ARG_TSK}"],"memory":512,"memoryReservation":512,"portMappings":[{"containerPort":18018}],"workingDirectory":"usr/ app","disableNetworking":false,"privileged":true,"healthCheck":{"command":[""],"interval":300,"timeout":60,"retries":5,"startPeriod":150}}]' | jq --raw-output '.taskDefinition.revision')
+
+echo "*** Task Definition *** > " $TASK_NAME:$TASK_VERSION 
+
+aws ecs list-services --cluster $ECS_CLUSTER_NAME  | grep $SERVICE_NAME || aws ecs create-service --service-name $SERVICE_NAME --cluster $ECS_CLUSTER_NAME --task-definition $TASK_NAME  --desired-count 1
+
+echo "*** creating service and task for {$TASK_NAME} ... ***" 
+
+export DEPLOYED_SERVICE=$(aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_NAME:$TASK_VERSION  | jq --raw-output  '.service.serviceName')
+
+echo "Deployment of {$DEPLOYED_SERVICE} complete"
